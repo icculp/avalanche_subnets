ed7efd3250bcf28842819c93285ba145bf51c5e0
Ian Culp <57159226+icculp@users.noreply.github.com>
2023-03-27 13:09:54 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-AVALANCHE_VERSION=v1.9.14
+AVALANCHE_VERSION=v1.9.16
 SUBNET_VERSION=v0.4.12
 docker build --build-arg AVALANCHE_VERSION=${AVALANCHE_VERSION} --build-arg SUBNET_VERSION=${SUBNET_VERSION} . -t icculp/avax_dfk:${AVALANCHE_VERSION}-${SUBNET_VERSION}
 docker image push icculp/avax_dfk:${AVALANCHE_VERSION}-${SUBNET_VERSION}
