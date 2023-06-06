84fa5bf5eeb948b2519e4f544a8c3392adf048ee
Ian Culp <icculp@gmail.com>
2023-06-06 17:58:38 +0000

    1.10.2

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 
 ```
-docker build --build-arg AVALANCHE_VERSION=v1.10.1 --build-arg SUBNET_VERSION=v0.5.1 . -t icculp/avax_dfk:v1.10.1-v0.5.1
+docker build --build-arg AVALANCHE_VERSION=v1.10.2 --build-arg SUBNET_VERSION=v0.5.1 . -t icculp/avax_dfk:v1.10.2-v0.5.1
 ```
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-AVALANCHE_VERSION=v1.10.1
+AVALANCHE_VERSION=v1.10.2
 SUBNET_VERSION=v0.5.1
 docker build --build-arg AVALANCHE_VERSION=${AVALANCHE_VERSION} --build-arg SUBNET_VERSION=${SUBNET_VERSION} . -t icculp/avax_dfk:${AVALANCHE_VERSION}-${SUBNET_VERSION}
 docker image push icculp/avax_dfk:${AVALANCHE_VERSION}-${SUBNET_VERSION}
